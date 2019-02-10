979d3c6f17db52fb3ad74ea74e6cfaa5a8afa2ec
annabellej <39597635+annabellej@users.noreply.github.com>
2019-02-09 16:51:51 -0800

    Update PartyInvitation.java

--- party-invitation/PartyInvitation.java
+++ party-invitation/PartyInvitation.java
@@ -4,12 +4,9 @@ import java.util.*;
 public class PartyInvitation {
     public static void takeOut(ArrayList<TreeSet<Integer>> a, ArrayList<Integer> c, int x)
     {
-        for (int i = 0; i < a.size(); i++)
-        {
+        for (int i = 0; i < a.size(); i++) {
             TreeSet<Integer> t = a.get(i);
-
-            if (t.contains(x))
-            {
+            if (t.contains(x)) {
                 t.remove(x);
                 c.set(i, c.get(i)-1);
             }
@@ -29,34 +26,27 @@ public class PartyInvitation {
         ArrayList<Integer> count = new ArrayList<>(); //stores the number of cows in each group
         ArrayList<TreeSet<Integer>> groups = new ArrayList<>(); //stores all the groups
 
-        for (int i = 0; i < numGroups; i++)
-        {
+        for (int i = 0; i < numGroups; i++) {
             groups.add(new TreeSet<>());
-
+            
             StringTokenizer st = new StringTokenizer(f.readLine());
             int c = Integer.parseInt(st.nextToken());
 
             count.add(i,c);
 
-            while (st.hasMoreTokens())
-            {
+            while (st.hasMoreTokens()) {
                 groups.get(i).add(Integer.parseInt(st.nextToken()));
             }
         }
 
         //check to make sure 1 is in at least one of the groups
-        for (TreeSet<Integer> t: groups)
-        {
+        for (TreeSet<Integer> t: groups) {
             if (t.contains(1))
-            {
                 takeOut(groups, count, 1); //take a certain number out from all the groups
-            }
         }
-
         invite++;
 
-        while (count.contains(1))
-        {
+        while (count.contains(1)) {
             int i = count.indexOf(1);
             TreeSet<Integer> g = groups.get(i);
 
@@ -64,7 +54,6 @@ public class PartyInvitation {
             takeOut(groups, count, num);
             invite++;
         }
-
         out.println(invite);
         out.close();
     }
